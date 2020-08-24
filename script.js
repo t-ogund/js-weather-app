eeb918971da29ad52713b05c059218be0abd73f6
Toye Ogundepo <ogundepo@gmail.com>
2020-08-23 23:59:46 -0500

    enhancements

--- script.js
+++ script.js
@@ -10,14 +10,99 @@ const narrative = document.getElementsByClassName("narrative");
 const icon = document.getElementsByClassName("icon");
 const card = document.getElementsByClassName("card");
 
+function onLoad() {
+    fetch("https://api.weatherbit.io/v2.0/forecast/daily?ip=auto&days=5&units=I&key=ec69804a082347b592321d0caf2b15cb")
+    .then(res => res.json())
+    .then(res => {
+        const fiveDayForecastLoad = res.data;
+        for (let i = 0; i < highTemp.length; i++) {
+            for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                highTemp[i].textContent = Math.floor(fiveDayForecastLoad[i].high_temp) + " / ";
+                console.log(fiveDayForecastLoad[i].high_temp)
+            }
+        }
+
+        for (let i = 0; i < lowTemp.length; i++) {
+            for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                lowTemp[i].textContent = Math.floor(fiveDayForecastLoad[i].low_temp);
+                console.log(fiveDayForecastLoad[i].low_temp)
+            }
+        }
+
+        for (let i = 0; i < condition.length; i++) {
+            for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                condition[i].textContent = fiveDayForecastLoad[i].weather.description
+            }
+        }
+
+        for (let i = 0; i < narrative.length; i++) {
+            for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                const windSpeed = Math.floor(fiveDayForecastLoad[i].wind_spd)
+ 
+                const windDirection = fiveDayForecastLoad[i].wind_cdir_full
+
+                narrative[i].textContent = `${windSpeed} MPH winds from the ${windDirection}`
+            }
+        }
+
+        for (let i = 0; i < imgSection.length; i++) {
+            //    console.log(imgSection[i])
+               for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                // console.log(fiveDayForecast[i].weather.icon)
+                imgSection[i].style.backgroundImage = `url("icons/icons/${fiveDayForecastLoad[i].weather.icon}.png")`
+               }
+           }
+
+           for (let k = 0; k < date.length; k++) {
+            console.log(date[k])
+            for (let l = 0; l < fiveDayForecastLoad.length; l++) {
+                const formattedDate = fiveDayForecastLoad[k].datetime.slice(6)
+                
+                console.log(formattedDate.replace("-", "/"))
+                date[k].textContent = formattedDate.replace("-", "/");
+            }
+        }
+
+        for (let i = 0; i < day.length; i++) {
+            for (let j = 0; j < fiveDayForecastLoad.length; j++) {
+                const ymd = fiveDayForecastLoad[j].datetime
+                console.log(ymd)
+                const workingOnDate = moment(ymd);
+                const dow = workingOnDate.day()
+                console.log(dow)
+
+                if (dow === 0) {
+                    day[j].textContent = "Sunday";
+                } else if (dow === 1) {
+                    day[j].textContent = "Monday";
+                } else if (dow === 2) {
+                    day[j].textContent = "Tuesday";
+                } else if (dow === 3) {
+                    day[j].textContent = "Wednesday";
+                } else if (dow === 4) {
+                    day[j].textContent = "Thursday";
+                } else if (dow === 5) {
+                    day[j].textContent = "Friday";
+                } else {
+                    day[j].textContent = "Saturday";
+                } 
+            } 
+        }
+    })
+    
+
+    
+}
+onLoad();
+
 submitButton.addEventListener("click", function() {
     event.preventDefault()
-    
+
     fetch(`https://api.weatherbit.io/v2.0/forecast/daily?&units=I&postal_code=${inputData.value}&country=US&key=ec69804a082347b592321d0caf2b15cb`)
     .then(response => response.json())
     .then(response => {
         const fiveDayForecast = response.data.slice(0, 5)
-        console.log(fiveDayForecast)
+        // console.log(fiveDayForecast)
 
         inputData.value = "";
 
@@ -94,7 +179,7 @@ submitButton.addEventListener("click", function() {
        }
     })
 
-    for (let i = 0; i < card.length; i++) {
-        card[i].style.display = "block";
-    }
+    // for (let i = 0; i < card.length; i++) {
+    //     card[i].style.display = "block";
+    // }
 })
